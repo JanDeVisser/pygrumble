097b53b88d0399139e1d10fcbb7a66c4b1df07b6
jan@chipmunk <jan@chipmunk>
2013-12-20 09:43:28 -0500

    xx

--- src/grumble/geopt.py
+++ src/grumble/geopt.py
@@ -313,25 +313,18 @@ class GeoBox(object):
                     self.ne().lon >= point.lon)
 
     def intersects(self, *other):
+        """
+            Check if two boxes collide.
+        """
         other = GeoBox(*other)
         if not(self and other):
             return False
         else:
-            """
-               /**
-               / * Check if two rectangles collide
-                * x_1, y_1, width_1, and height_1 define the boundaries of the first rectangle
-                * x_2, y_2, width_2, and height_2 define the boundaries of the second rectangle
-                */
-               boolean rectangle_collision(float x_1, float y_1, float width_1, float height_1, float x_2, float y_2, float width_2, float height_2)
-               {
-                 return !(x_1 > x_2+width_2 || x_1+width_1 < x_2 || y_1 > y_2+height_2 || y_1+height_1 < y_2);
-               }
-            """
-            return not(self.sw().lat <= other.sw().lat and 
-                    self.sw().lon <= other.sw().lon and
-                    self.ne().lat >= other.ne().lat and
-                    self.ne().lat >= other.ne().lat)
+            return not(
+                self.sw().lon > other.ne().lon or
+                self.ne().lon < other.sw().lon or
+                self.sw().lat > other.ne().lat or 
+                self.ne().lat < other.sw().lat )
                     
     def span(self):
         return GeoPt(
